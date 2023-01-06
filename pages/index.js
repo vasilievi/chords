0a2280ec5fe5de3efde54a9adc670d27f11d28d0
vasilievi <vasilievi@outlook.com>
2023-01-06 11:16:11 +0300

    -

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
-import "bootstrap/dist/css/bootstrap.css"
-import { useState } from 'react';
-
-function homepage(params) {
-    const [textState, setTextState] = useState('init text');
-
-    const onClickSend = async (e) => {
-        e.target.disabled = true
-        const res = await fetch('/api/addData', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({ "text": textState })
-        });
-        const resJson = await res.json()
-        console.log(resJson);
-        e.target.disabled = false
-    }
-
-    return (
-        <div className="border border-success m-3 p-3">
-            <h1 className="text-primary">Welcome home!</h1>
-            <input placeholder="text"
-                value={textState}
-                onChange={(e) => {
-                    setTextState(e.target.value)
-                }} />
-            <button className="btn btn-success" onClick={onClickSend}>Send</button>
-        </div>
+function index(props) {
+    return(
+        <h1>Chords</h1>
     )
 }
 
-export default homepage
+export default index
