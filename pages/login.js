15b1e6ebfa151aece7ca7ebecfff1e05c1fe650a
vasilievi <vasilievi@outlook.com>
2023-01-12 22:23:44 +0300

    -

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,46 @@
 import Navbar from "../components/Navbar.js"
+import { useState } from 'react';
 
 export default function login(props) {
+
+    const [user, setUser] = useState({
+        phonenumber: '',
+        code: ''
+    });
+
+    const [codeSent, setCodeSent] = useState(false)
+    const [confirmed, setConfirmed] = useState(false)
+
+    const getCode = async () => {
+        console.log('getCode');
+        let response = await fetch('/api/getCode', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(user)
+        });
+
+        if (response.status === 200) {
+            setCodeSent(true)
+        }
+    }
+
+    const confirm = async () => {
+        console.log('confirm');
+        let response = await fetch('/api/confirm', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(user)
+        });
+
+        if (response.status === 200) {
+            setConfirmed(true)
+        }
+    }
+
     return (
         <div className="bg-black">
             <Navbar logo="Best chords" />
@@ -19,19 +59,38 @@ export default function login(props) {
                                                 <div className="mb-3 row">
                                                     <label className="form-label bg-black text-white">Phone number</label>
                                                     <div className="col-auto">
-                                                        <input className="form-control" type="number" placeholder="9876543210" max="9999999999" />
+                                                        <input className="form-control" type="number"
+                                                            placeholder="9876543210"
+                                                            max="9999999999"
+                                                            disabled={codeSent}
+                                                            value={user.phonenumber}
+                                                            onChange={(e) => {
+                                                                setUser({ ...user, phonenumber: e.target.value })
+                                                            }} />
                                                     </div>
                                                     <div className="col-auto">
-                                                        <button type="button" className="btn btn-outline-warning">Get code</button>
+                                                        <button type="button" className="btn btn-outline-warning"
+                                                            disabled={codeSent}
+                                                            onClick={getCode}
+                                                        >Get code</button>
                                                     </div>
                                                 </div>
                                                 <div className="mb-3 row">
                                                     <label className="form-label bg-black text-white">Code</label>
                                                     <div className="col-auto">
-                                                        <input className="form-control" type="number" placeholder="1234" max="9999" />
+                                                        <input className="form-control" type="number"
+                                                            placeholder="1234"
+                                                            max="9999"
+                                                            disabled={confirmed}
+                                                            value={user.code}
+                                                            onChange={(e) => {
+                                                                setUser({ ...user, code: e.target.value })
+                                                            }} />
                                                     </div>
                                                     <div className="col-auto">
-                                                        <button type="button" className="btn btn-outline-warning">Confirm</button>
+                                                        <button type="button" className="btn btn-outline-warning"
+                                                            disabled={confirmed}
+                                                            onClick={confirm}>Confirm</button>
                                                     </div>
                                                 </div>
                                             </form>
