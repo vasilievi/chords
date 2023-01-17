cf1cd3805c60cca69d845d328cacebb38ed3852a
vasilievi <vasilievi@outlook.com>
2023-01-17 06:41:52 +0300

    -

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer(props) {
     return (
         <div className="bg-black text-light">
             <hr></hr>
-            <p className="text-center">© 2023 Chords-one.Vercel.app</p>
+            <p className="text-center">© 2023 Chords-one.Vercel.app (230117)</p>
         </div>
     )
 }
