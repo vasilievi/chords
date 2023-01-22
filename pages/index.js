00abb0b820967c088c4bda294fc147ed78f37618
vasilievi <vasilievi@outlook.com>
2023-01-22 17:16:56 +0300

    -

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Navbar from "../components/Navbar.js"
 import Footer from "../components/Footer.js"
 import Carousel from "../components/Carousel.js"
+import Link from 'next/link'
 
-export default function homePage(props) {
+
+export default function homePage() {
 
     return (
         <div className="bg-black  vh-100">
             <Navbar logo="Best chords" />
             <Carousel />
+            <div className="bg-black d-flex justify-content-center">
+                <Link
+                    className='h1 text-warning text-center m-3 no-underline'
+                    href={'/songs'}>
+                    Let's play!
+                </Link>
+            </div>
             <Footer />
         </div>
     )
