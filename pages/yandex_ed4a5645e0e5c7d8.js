0482b59ed1b0e5cfe7b9a2cf4360678b663843a8
vasilievi <vasilievi@outlook.com>
2023-01-27 06:36:03 +0300

    -

---
+++ pages/yandex_ed4a5645e0e5c7d8.js
@@ -0,0 +1,11 @@
+export default function yandex() {
+
+    return (
+        <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            </head>
+            <body>Verification: ed4a5645e0e5c7d8</body>
+        </html>
+    )
+}
