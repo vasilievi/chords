16ec04784e52d534742bad1b27aaad93c808ca0f
vasilievi <vasilievi@outlook.com>
2023-01-27 06:49:22 +0300

    -

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Navbar from "../components/Navbar.js"
 import Footer from "../components/Footer.js"
 import Carousel from "../components/Carousel.js"
 import Link from 'next/link'
+import Head from 'next/head'
 
 
 export default function homePage() {
 
     return (
         <div className="bg-black  vh-100">
+            <Head>
+                <meta name="yandex-verification" content="ed4a5645e0e5c7d8" />
+            </Head>
             <Navbar logo="Best chords" />
             <Carousel />
             <div className="bg-black d-flex justify-content-center">
