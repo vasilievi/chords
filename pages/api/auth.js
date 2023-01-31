19d02229e3c6df87d3175e72e471cb8846c23f87
vasilievi <vasilievi@outlook.com>
2023-01-31 06:29:49 +0300

    -

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         token: token
     }
 
-    let resPhoneVerify = await fetch(`https://api.nerotech.ru/api/v1/call?service_id=386&secret_key=caa325a407d2d181ed1fffa14970a053&phone=7${req.body.phonenumber}`)
+    let resPhoneVerify = await fetch(`https://api.nerotech.ru/api/v1/call?service_id=386&secret_key=${process.env.NEROTECH_KEY}&phone=7${req.body.phonenumber}`)
     let resPhoneVerifyJson = await resPhoneVerify.json()
     if (resPhoneVerifyJson.code) {
         updateUser.code = resPhoneVerifyJson.code
