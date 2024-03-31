17e707dc5d44025de83706abb03b503a192dba17
Igor Vasiliev <75659259+vasilievi@users.noreply.github.com>
2024-03-31 19:43:54 +0300

    Update HighlightText.js

--- components/HighlightText.js
+++ components/HighlightText.js
@@ -8,7 +8,7 @@ export default function HighlightText(props) {
 
     
   const highlightText = () => {
-    const regex = new RegExp(`[ \n][A-G][ \n]|[ \n][A-G][#bm67][ \n]|[ \n][A-G][#bm67][#bm67][ \n]`, 'g');
+    const regex = new RegExp(`[ \n][A-G][/][*][ \n]|[ \n][A-G][ \n]|[ \n][A-G][#bm67][ \n]|[ \n][A-G][#bm67][#bm67][ \n]`, 'g');
     const text = props.text
     const highlightedText = text.replace(regex, '<mark class="bg-black text-warning">$&</mark>');
     const pre = document.getElementById('pre')
